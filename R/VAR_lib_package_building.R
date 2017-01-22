a1ed7fe437286bc462e82b0e03a47da29506b370
Xin Ding <dingx92@gmail.com>
2017-01-22 14:02:15 -0600

    v1

--- R/VAR_lib_package_building.R
+++ R/VAR_lib_package_building.R
@@ -12,7 +12,6 @@ library(abind)
 library(compiler)
 enableJIT(3)
 
-
 weight_epanechnikovker <- function(t, m, n, b)
 {
   v = (t-m/n)/b
@@ -36,7 +35,7 @@ vc_weight_epanechnikovker=cmpfun(vc_weight_epanechnikovker)
 
 #' fit a TV_VAR model
 #' @param it Specify the time point we are researching
-#' @param X T*d array which stores the data of the time series. T is the length and d is the dimension.
+#' @param X T*d array which stores the data of the time series. T is the length of time and d is the dimension.
 #' @param tau penalty parameter
 #' @param b bandwidth
 #' @return Estimation of transition matrix at time t
@@ -61,9 +60,6 @@ tvvar_fit<-function(it,X,tau,b){
   s0_1=crossprod(X[2:T,]*drop(ker_weight[2:T]),X[1:(T-1),])
   #solve linear programming
   f.obj=rep(1,2*d)
-  # f.con1=cbind(-s10,s10)
-  # f.con2=cbind(s00,-s10)
-  # f.con=rbind(f.con1,f.con2)
   f.con=matrix(rep(0,4*d^2),nrow=2*d)
   f.con[1:d,1:d]=-s10
   f.con[1:d,(d+1):(2*d)]=s10
@@ -85,7 +81,7 @@ tvvar_fit<-function(it,X,tau,b){
 
 #' fit a Ridge_VAR model based on LS with l2 penalty
 #' @param it Specify the time point we are researching
-#' @param X T*d array which stores the data of the time series. T is the length and d is the dimension.
+#' @param X T*d array which stores the data of the time series. T is the length of time and d is the dimension.
 #' @param lam penalty parameter
 #' @param b bandwidth
 #' @return Estimation of transition matrix at time t
@@ -107,7 +103,7 @@ lsvar_fit<-function(it,X,lam,b){
 
 #' fit a MLE_VAR model based on MLE
 #' @param it Specify the time point we are researching
-#' @param X T*d array which stores the data of the time series. T is the length and d is the dimension.
+#' @param X T*d array which stores the data of the time series. T is the length of time and d is the dimension.
 #' @param b bandwidth
 #' @return Estimation of transition matrix at time t
 mlevar_fit<-function(it,X,b){
@@ -115,8 +111,6 @@ mlevar_fit<-function(it,X,b){
   d=dim(X)[2]
   #compute the kernel weights for t_i
   ker_weight=vc_weight_epanechnikovker(it/T,1:T,T,b)
-  # #compute the kernel weights for t_{i-1}
-  # ker_weight_1=vc_weight_epanechnikovker((it-1)/T,1:T,T,b)
   W1=crossprod(X[2:T,]*drop(ker_weight[2:T]),X[1:(T-1),])
   W2=crossprod(X[1:(T-1),]*drop(ker_weight[2:T]),X[1:(T-1),])
   A_hat_MLE=tcrossprod(W1,chol2inv(chol(W2)))
@@ -134,7 +128,7 @@ abso<-function(x){
 }
 #' fit a Lasso_VAR model based on LS with l1 penalty
 #' @param it Specify the time point we are researching
-#' @param X T*d array which stores the data of the time series. T is the length and d is the dimension.
+#' @param X T*d array which stores the data of the time series. T is the length of time and d is the dimension.
 #' @param lam shrinkage parameter
 #' @param b bandwidth
 #' @param alpha stepsize
@@ -142,7 +136,6 @@ abso<-function(x){
 #' @param A_oldold initial matrix 2
 #' @return Estimation of transition matrix at time t
 lavar_fit<-function(it,X,lam,b,alpha,A_old,A_oldold){
-  # alpha=1/max(svd(crossprod(X,X))$d)
   T=dim(X)[1]
   d=dim(X)[2]
   #compute the kernel weights for t_i
